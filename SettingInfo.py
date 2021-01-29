81a9ee8b7db45c83054083c6403c7a183119add1
lijie3912 <lijie@xmfunny.com>
2021-01-29 15:51:10 +0800

    20210129 修改cookie

--- SettingInfo.py
+++ SettingInfo.py
@@ -32,11 +32,11 @@ if platform.system().lower() == "windows":  # 判断当前运行环境为windows
     DEVICEINFO = r"E:\DispatchScript\deviceInfo.json"
     SuccessfulDevices =r"E:\DispatchScript\SuccessfulDevices.json"
     ATXPROVIDER_URL = "http://10.30.20.29:3500/app/install?udid="
-    userid = 'user_id="2|1:0|10:1609209313|7:user_id|32:c2hpd2tpbmdAYW5vbnltb3VzLmNvbQ==|a87978b7aca1d6a488f923baf30bdb3524590fdd47527295bb0cac0e89735bff"; Path=/; Domain=10.30.20.29; Expires=Fri, 23 Oct 2020 06:13:43 GMT;'
+    userid = 'user_id="2|1:0|10:1611821933|7:user_id|32:c2hpd2tpbmdAYW5vbnltb3VzLmNvbQ==|371fe99f038658345db7da6609d10bbeaa2d0efe0804f35000a4ba6bbe8f5a3c"; Path=/; Domain=10.30.20.29; Expires=Fri, 23 Oct 2020 06:13:43 GMT;'
 elif platform.system().lower() == "linux":  # 判断当前运行环境为linux时
     outpath = "/DownloadAPK"
     ATXSERVER = "http://10.30.20.29:4000/"
     DEVICEINFO = "/var/jenkins_home/deviceInfo.json"
     SuccessfulDevices = "/SuccessfulDevices.json"
     ATXPROVIDER_URL = "http://10.30.20.29:3500/app/install?udid="
-    userid = 'user_id="2|1:0|10:1609209313|7:user_id|32:c2hpd2tpbmdAYW5vbnltb3VzLmNvbQ==|a87978b7aca1d6a488f923baf30bdb3524590fdd47527295bb0cac0e89735bff"; Path=/; Domain=10.30.20.29; Expires=Fri, 23 Oct 2020 06:13:43 GMT;'
+    userid = 'user_id="2|1:0|10:1611821933|7:user_id|32:c2hpd2tpbmdAYW5vbnltb3VzLmNvbQ==|371fe99f038658345db7da6609d10bbeaa2d0efe0804f35000a4ba6bbe8f5a3c"; Path=/; Domain=10.30.20.29; Expires=Fri, 23 Oct 2020 06:13:43 GMT;'
